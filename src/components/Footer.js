0c3c616297d2308cb17e778dd49cb24833818ca6
Andres Garza <74g4bond@gmail.com>
2023-05-10 20:03:40 -0500

    ver 6

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return(
         <footer className="footer">
             <div></div><a href="https://github.com/7agabond" target="_blank"><img src="./images/25231.png" className="footer--gitimg"/></a>
-            <a href="https://www.linkedin.com/in/andres-garza-bb6966233/" target="_blank"><img src="../images/174857.png" className="footer--linkedinimg"/></a>
+            <a href="https://www.linkedin.com/in/andres-garza-bb6966233/" target="_blank"><img src="./images/174857.png" className="footer--linkedinimg"/></a>
             <small className="footer--text">© 2023 Garza development. All rights reserved.</small>
         </footer>
     )
