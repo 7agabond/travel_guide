b62fa8363580765118403c30e887526fb94ba830
Andres Garza <74g4bond@gmail.com>
2023-05-10 19:58:25 -0500

    ver 4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react"
+import GitLogo from "./images/25231.png"
+import LinkedInLogo from "./images/174857.png"
 
 export default function Footer() {
     return(
         <footer className="footer">
-            <div></div><a href="https://github.com/7agabond" target="_blank"><img src="./images/25231.png" className="footer--gitimg"/></a>
-            <a href="https://www.linkedin.com/in/andres-garza-bb6966233/" target="_blank"><img src="./images/174857.png" className="footer--linkedinimg"/></a>
+            <div></div><a href="https://github.com/7agabond" target="_blank"><img src={GitLogo} className="footer--gitimg"/></a>
+            <a href="https://www.linkedin.com/in/andres-garza-bb6966233/" target="_blank"><img src={LinkedInLogo} className="footer--linkedinimg"/></a>
             <small className="footer--text">© 2023 Garza development. All rights reserved.</small>
         </footer>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react"
+import NavLogo from "./images/dlf.pt-view-icon-png-1566250.png"
 
 export default function Nav() {
     return(
         <nav className="nav--container">
-            <img src="./images/dlf.pt-view-icon-png-1566250.png" className="nav--img" />
+            <img src={NavLogo} className="nav--img" />
             <h1 className="nav--text">my travel bucket list.</h1>
         </nav>
     )
