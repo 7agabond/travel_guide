66179bda11495f6aa715203f78e2ca16d84d5bad
Andres Garza <74g4bond@gmail.com>
2023-05-10 20:00:08 -0500

    ver 5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 import React from "react"
-import NavLogo from "./images/dlf.pt-view-icon-png-1566250.png"
 
 export default function Nav() {
     return(
         <nav className="nav--container">
-            <img src={NavLogo} className="nav--img" />
+            <img src="./images/dlf.pt-view-icon-png-1566250.png" className="nav--img" />
             <h1 className="nav--text">my travel bucket list.</h1>
         </nav>
     )
